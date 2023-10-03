8558dddf69591a914bf735041acc7934dcb8188c
msantism <mauriciosantism@gmail.com>
2023-10-03 20:31:27 -0300

    Se crea el modelo annotation

---
+++ app/Models/annotation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class annotation extends Model
+{
+    use HasFactory;
+}
